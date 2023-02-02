ac635bedd62b53769aeef3d4cc35e532c1679410
Heloisa-Oliveira <heloisa.oliveira@sou.inteli.edu.br>
2023-02-02 11:54:09 -0300

    Heloisa_Oliveira
    Mudei a movimentação do Dragão, o quão rápido ele vai pra cima e pra baixo, além da maior velocidade do cenário.

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 1 #define se o jogador está jogando ou não
+var vscore = 0 #define a pontuação inicial do jogador
+var x = 4 #define a velocidade com que o cenário passa para o jogador
+var y = 1.5 #define o quanto ele cai sem apertar nenhuma das setas
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -41,11 +41,11 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 6
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 8
 			
 	elif status == 0: # parado
 		
